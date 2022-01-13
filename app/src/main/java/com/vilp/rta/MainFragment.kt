6f49ab26d4302a6a88ecac29d6a809226d6d8b5a
filipe-varela <bfagdsv250@gmail.com>
2022-01-13 13:18:26 +0000

    Added the navigation component and the MainFragment ( #1 )

---
+++ app/src/main/java/com/vilp/rta/MainFragment.kt
@@ -0,0 +1,20 @@
+package com.vilp.rta
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+
+class MainFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? = inflater.inflate(R.layout.fragment_main, container, false).also {
+        it.findViewById<TextView>(R.id.txt_test).text = "Olá"
+    }
+
+}
