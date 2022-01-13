f41d7f941a4c1896571e809390f9e214ccf36751
filipe-varela <bfagdsv250@gmail.com>
2022-01-13 23:29:07 +0000

    Navigation Component Fragments Implemented ( #1 )

--- app/src/main/java/com/vilp/rta/MainFragment.kt
+++ app/src/main/java/com/vilp/rta/MainFragment.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
 
 class MainFragment : Fragment() {
 
@@ -15,6 +17,9 @@ class MainFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? = inflater.inflate(R.layout.fragment_main, container, false).also {
         it.findViewById<TextView>(R.id.txt_test).text = "Olá"
+        it.findViewById<Button>(R.id.btn_test).setOnClickListener { _ ->
+            Navigation.findNavController(it).navigate(R.id.action_mainFragment_to_menuFragment)
+        }
     }
 
 }
