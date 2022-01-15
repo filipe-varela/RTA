7d4a515f38ed801a12c0e7f6b16529427bb294c1
filipe-varela <bfagdsv250@gmail.com>
2022-01-15 11:16:37 +0000

    Fragment menu done ( #1 )

--- app/src/main/java/com/vilp/rta/MenuFragment.kt
+++ app/src/main/java/com/vilp/rta/MenuFragment.kt
@@ -5,55 +5,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.Navigation
+import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [MenuFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class MenuFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? = inflater.inflate(R.layout.fragment_menu, container, false).also {
+        it.findViewById<Button>(R.id.menu_btn_new).setOnClickListener { _ ->
+            Navigation.findNavController(it).navigate(R.id.action_menuFragment_to_novaEntradaFragment)
+        }
+        it.findViewById<Button>(R.id.menu_btn_exit).setOnClickListener {
+            activity?.finishAffinity()
+        }
+        it.findViewById<ExtendedFloatingActionButton>(R.id.menu_efab).setOnClickListener { _ ->
+            Navigation.findNavController(it).navigate(R.id.action_menuFragment_to_outrosFragment)
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_menu, container, false)
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment MenuFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            MenuFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
